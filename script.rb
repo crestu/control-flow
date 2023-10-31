60956298a5ee38162c3be9836af0903704ac4e16
Creed Studenski <creedstudenski@Creeds-MacBook-Air.local>
2023-10-31 14:11:06 -0500

    control-flow project

---
+++ script.rb
@@ -0,0 +1,36 @@
+#CREED AND JASON!
+
+#1
+print "what number is less than 10?"
+num = Integer(gets.chomp)
+if num<10
+    print"true!"
+elsif num>10
+    print "nope!"
+else
+    print"you chose 10!"
+end
+
+#2 
+school = false
+unless school
+    print "snowday!"
+else 
+    print "welcome to school!"
+end
+
+#3
+
+test_1= 1>2
+
+test_2= 2==3
+
+test_3= (5**2) == 25
+
+#4
+
+test_1= true || !false
+
+test_2= (1<2 && 3<4) || true
+
+test_3= !true && 1<2
